b999d2e7b35293353f21d0d8ff63ca91334952b9
Soumya Deb <debloper@gmail.com>
2016-03-08 17:41:21 +0530

    Task runner rigging with basic login page

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 
-gulp.task('libs', function() {
+gulp.task('jsLibs', function() {
   return gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/patternfly/dist/js/patternfly.min.js'
@@ -10,8 +11,41 @@ gulp.task('libs', function() {
   .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('build', ['libs'], function () {
-  // Umbrella tasks
+gulp.task('cssLibs', function() {
+  return gulp.src([
+    'node_modules/patternfly/dist/css/patternfly.min.css',
+    'node_modules/patternfly/dist/css/patternfly-additions.min.css'
+  ])
+  .pipe(concat('libs.css'))
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/styles/*.css')
+  .pipe(concat('app.css'))
+  .pipe(minify())
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/index.html')
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/*.*')
+  .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/styles/*.js')
+  .pipe(concat('app.js'))
+  .pipe(minify())
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build', ['jsLibs', 'cssLibs', 'css', 'html', 'images', 'scripts'], function () {
+  console.log("Build succesful; build artifacts are under ./dist")
 });
 
 gulp.task('default', function() {
